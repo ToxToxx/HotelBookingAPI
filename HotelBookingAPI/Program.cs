b262f851e93f8bfe3fcfdd3cfe90af6d20fee739
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-12-30 16:12:00 +0300

    add db

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<APIContext>
+    (opt => opt.UseInMemoryDatabase("BookingDB"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
