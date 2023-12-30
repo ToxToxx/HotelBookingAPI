fb740e0e117c5870d42014851ff47c5ce7d9b64b
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-12-30 16:11:47 +0300

    add context

--- HotelBookingAPI/Data/APIContext.cs
+++ HotelBookingAPI/Data/APIContext.cs
@@ -3,7 +3,13 @@ using HotelBookingAPI.Models;
 
 namespace HotelBookingAPI.Data
 {
-    public class APIContext
+    public class APIContext : DbContext
     {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public APIContext(DbContextOptions<APIContext> options) : base(options)
+        {
+
+        }
     }
 }
