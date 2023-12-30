f70992dc06c073ff4de15aac455ec749cafdac65
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-12-30 16:12:08 +0300

    made controller for hotel booking

---
+++ HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly APIContext _context;
+
+        public HotelBookingController(APIContext context) 
+        {
+            _context = context;
+        }
+
+        //create / edit
+        [HttpPost]
+        public JsonResult CreatedEdit(HotelBooking hotelBooking)
+        {
+            if(hotelBooking.Id == 0)
+            {
+                _context.Bookings.Add(hotelBooking);
+            } else
+            {
+                var bookingInDB = _context.Bookings.Find(hotelBooking.Id);
+
+                if (bookingInDB == null)
+                    return new JsonResult(NotFound());
+
+                bookingInDB = hotelBooking;
+            }
+            _context.SaveChanges();
+            return new JsonResult(Ok(hotelBooking));
+        }
+    }
+}
